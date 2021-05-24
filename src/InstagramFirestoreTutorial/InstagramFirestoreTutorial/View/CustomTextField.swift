1d03e9ce1fa35ae85c2c8f2d668055e70a73ba5a
Satoshi Shimamura <>
2021-05-25 08:57:23 +0900

    note

---
+++ src/InstagramFirestoreTutorial/InstagramFirestoreTutorial/View/CustomTextField.swift
@@ -0,0 +1,34 @@
+//
+//  CustomTextField.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by SatoshiShimamura on 2021/05/25.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        leftView = spacer
+        leftViewMode = .always
+        
+        borderStyle = .none
+        textColor = .white
+        keyboardAppearance = .dark
+        keyboardType = .emailAddress
+        backgroundColor = UIColor(white: 1, alpha: 0.1)
+        setHeight(50) // paddingvertical
+        attributedPlaceholder =
+            NSAttributedString(string: placeholder,
+                               attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
